19dc8840750792880a29a56e56d4fdfdadebb044
Josh <jm8hjkl@gmail.com>
2022-02-17 16:58:41 -0500

    made it compile

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "Godot 4.0 alpha 1";
 
+  # https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/tools/godot/default.nix
+
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   inputs.src = {
@@ -10,10 +12,15 @@
 
   outputs = { self, src, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
+        options = {
+          touch = true;
+          pulseaudio = false;
+          udev = true;
+        };
     in {
       defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
-        name = "godot4";
+        pname = "godot";
+        version = "4.0-alpha2";
 
         src = src;
 
@@ -35,15 +42,39 @@
           freetype
           openssl
           alsa-lib
+          alsa-lib.dev
           libpulseaudio
           libGLU
           zlib
           yasm
         ];
 
+        patches = [ ./pkg_config_additions.patch ./dont_clobber_environment.patch ];
+
+        outputs = ["out" "dev" "man"];
+
+        sconsFlags = "target=release_debug platform=linuxbsd";
+
+        preConfigure = ''
+          sconsFlags+=" ${
+            pkgs.lib.concatStringsSep " "
+            (pkgs.lib.mapAttrsToList (k: v: "${k}=${builtins.toJSON v}") options)
+          }"
+        '';
 
         installPhase = ''
-          cp $src/README.md $out
+          mkdir -p "$out/bin"
+          cp bin/godot.* $out/bin/godot
+          mkdir "$dev"
+          cp -r modules/gdnative/include $dev
+          mkdir -p "$man/share/man/man6"
+          cp misc/dist/linux/godot.6 "$man/share/man/man6/"
+          mkdir -p "$out"/share/{applications,icons/hicolor/scalable/apps}
+          cp misc/dist/linux/org.godotengine.Godot.desktop "$out/share/applications/"
+          cp icon.svg "$out/share/icons/hicolor/scalable/apps/godot.svg"
+          cp icon.png "$out/share/icons/godot.png"
+          substituteInPlace "$out/share/applications/org.godotengine.Godot.desktop" \
+            --replace "Exec=godot" "Exec=$out/bin/godot"
         '';
       };
     };
